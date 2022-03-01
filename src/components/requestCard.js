8e9610ce6faa3c510b7f985006687ec82e75262c
Jas1nskas <Edvinasjasinskas@gmail.com>
2022-03-01 13:05:56 +0200

    first commit

---
+++ src/components/requestCard.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+const request = ({site}) => {
+
+    const [status, setStatus] = useState(null)
+    const [req, setReq] = useState(0)
+
+    function send() {
+        const options = {
+            method: "GET",
+            mode: "no-cors"
+        }
+
+        fetch(`http://${site}`, options)
+            .then(res => res.text())
+            .then(html => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, 'text/html');
+                console.log(doc, site)
+                setStatus(true)
+                setReq(req + 1)
+            }).catch(e => {
+            setStatus(false)
+            console.log('ERROR!', e)
+        })
+    }
+
+    setInterval(send, 5000)
+    useEffect(() => {
+        send()
+    },[req])
+
+    return (
+        <div className="flex space-a border">
+            <div className="site">
+                <h5>{site}</h5>
+            </div>
+            <h5 style={{color: status ? "green" : "red"}}>{status ? "Online" : "Offline"}</h5>
+            <h5>{req}</h5>
+        </div>
+    );
+};
+export default request;
